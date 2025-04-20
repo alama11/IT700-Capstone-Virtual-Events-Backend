007b08b7a455eb4758b83f8ef02eef4f5f033dcc
Aslat-Education <inquiry.AslatGuidance@gmail.com>
2025-04-19 22:21:26 -0600

    Initial Spring Boot project connected to GitHub

---
+++ src/main/java/com/it700/virtualevents/demo/VirtualEventsApplication.java
@@ -0,0 +1,13 @@
+package com.it700.virtualevents.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualEventsApplication.class, args);
+	}
+
+}
